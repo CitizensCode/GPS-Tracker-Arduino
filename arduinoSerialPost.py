aea2d3c25d9938df85d8544bd9bab8d76ed4c65a
Ryan Brideau <ryan.brideau@gmail.com>
2015-03-23 21:49:33 -0300

    Added logging to see POST request

--- arduinoSerialPost.py
+++ arduinoSerialPost.py
@@ -1,10 +1,22 @@
 import serial
 import requests
+import logging
+import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
 
+# Connect to the Arduino's serial port
 ser = serial.Serial('/dev/tty.usbmodem1451', 115200)
 
 postURL = "http://citizenscode-gpstracker-api.herokuapp.com/"
 
+# Initialize logging
+logging.basicConfig() 
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+# Loop and post the lat/lng
 while True:
     coord = ser.readline()
     coord = coord.strip('\n\r').split(',')
